3b2d194472c824adb7f6820e72d17a376d79ccda
“[Benard <ngwabern@gmail.com>
2023-11-15 22:45:42 -0600

    region second changes

--- terraform/eks-cluster-ec2/vpc.tf
+++ terraform/eks-cluster-ec2/vpc.tf
@@ -142,22 +142,22 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "private-us-east-1a" {
+resource "aws_route_table_association" "private-ca-central-1a" {
   subnet_id      = aws_subnet.private-ca-central-1a.id
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "private-us-east-1b" {
+resource "aws_route_table_association" "private-ca-central-1b" {
   subnet_id      = aws_subnet.private-ca-central-1b.id
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "public-us-east-1a" {
+resource "aws_route_table_association" "public-ca-central-1a" {
   subnet_id      = aws_subnet.public-ca-central-1a.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public-us-east-1b" {
+resource "aws_route_table_association" "public-ca-central-1b" {
   subnet_id      = aws_subnet.public-ca-central-1b.id
   route_table_id = aws_route_table.public.id
 }
